fe39041a7d09c0cbca68f1b5bd2dec21cb82a788
Thomas McCann <thomas.paul.mccann@gmail.com>
2022-11-18 22:00:11 +0000

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    const UpdatedBooks = [...books, { id: 123, title }];
+    setBooks(UpdatedBooks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {books.length}
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
