c6490c75a898f5ac030a55c5a6a58634e5843986
Thomas McCann <thomas.paul.mccann@gmail.com>
2022-11-19 14:15:29 +0000

    Implemented-delete-and-edit-book

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import BookList from './components/BookList';
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const UpdatedBooks = [
       ...books,
@@ -19,7 +26,7 @@ function App() {
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,5 +1,35 @@
-function BookShow({ book }) {
-  return <div className="book-show">{book.title}</div>;
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
+function BookShow({ book, onDelete }) {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit book={book} />;
+  }
+
+  return (
+    <div className="book-show">
+      {content}
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default BookShow;
