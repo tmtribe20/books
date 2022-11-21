5d3db6af847e773f1ebe283f1bd9d09c1ba325ee
Thomas McCann <thomas.paul.mccann@gmail.com>
2022-11-21 15:52:01 +0000

    implemented-use-effect-and-use-callback

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,20 +1,21 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import BooksContext from '../context/books';
 import BookEdit from './BookEdit';
 
-function BookShow({ book, onDelete, onEdit }) {
+function BookShow({ book }) {
   const [showEdit, setShowEdit] = useState(false);
+  const { deleteBookById } = useContext(BooksContext);
 
   const handleDeleteClick = () => {
-    onDelete(book.id);
+    deleteBookById(book.id);
   };
 
   const handleEditClick = () => {
     setShowEdit(!showEdit);
   };
 
-  const handleSubmit = (id, newTitle) => {
+  const handleSubmit = () => {
     setShowEdit(false);
-    onEdit(id, newTitle);
   };
 
   let content = <h3>{book.title}</h3>;
@@ -24,7 +25,7 @@ function BookShow({ book, onDelete, onEdit }) {
 
   return (
     <div className="book-show">
-      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />{' '}
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
